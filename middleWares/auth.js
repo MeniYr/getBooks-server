6c67e579a67e085b280db30c37dd0b5824e59397
meni rotblat <meny1717@gamil.com>
2022-07-31 00:43:23 +0300

    l

--- middleWares/auth.js
+++ middleWares/auth.js
@@ -4,7 +4,7 @@ exports.auth = async (req, res, next) => {
     let token = req.header("x-api-key")
     if (!token) return res.status(401).json({ msg: "tokan required" })
     try {
-        console.log(token)
+    
         let decodeToken = jwt.verify(token, "meni")
         req.tokenData = decodeToken;
         next()
